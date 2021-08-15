6d2f0f9bbdf99f06b9bdeebe5a6335d8b57cc904
jeffersonrcgouveia <jeffersonrcgouveia@gmail.com>
2021-08-15 12:43:41 +0100

    Added an example for the common events and listeners added.

--- Examples/Scripts/ScriptableEventsLogger.cs
+++ Examples/Scripts/ScriptableEventsLogger.cs
@@ -6,6 +6,18 @@ namespace ScriptableEvents.Examples.Scripts
 	{
 		public void LogScriptableEvent() => Debug.Log("ScriptableEvent");
 
-		public void LogGameObjectScriptableEvent(GameObject go) => Debug.Log($"GameObjectScriptableEvent: {go}");
+		public void LogGameObject(GameObject gameObject) => Debug.Log($"GameObject: {gameObject}");
+
+		public void LogTransform(Transform transform) => Debug.Log($"Transform: {transform}");
+
+		public void LogVector3(Vector3 vector3) => Debug.Log($"Vector3: {vector3}");
+
+		public void LogInt(int number) => Debug.Log($"Int: {number}");
+
+		public void LogFloat(float number) => Debug.Log($"Float: {number}");
+
+		public void LogBool(bool flag) => Debug.Log($"Bool: {flag}");
+
+		public void LogString(string text) => Debug.Log($"String: {text}");
 	}
 }
